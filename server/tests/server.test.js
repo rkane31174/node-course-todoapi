2db516354685c5cb581668f87f2c6bd1033605cc
Rkane <rkane@classlink.com>
2018-11-27 10:26:48 -0500

    Test DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -103,3 +103,45 @@ describe('GET /todos/:id', () => {
             .end(done);
     });
 });
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        var hexId = todos[0]._id.toHexString();
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+            })
+            .end((err, res) => {
+                if(err){
+                    return done(err);
+                }
+                request(app)
+                    .get(`/todos/${hexId}`)
+                    .expect((res) => {
+                        expect(404)
+                    })
+                    .end(done);
+        });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        var hexId = new ObjectID();
+        request(app)
+            .delete(`/todos/${hexId.toHexString()}`)
+            .expect((res) => {
+                expect(404)
+            })
+            .end(done);
+    });
+
+    it('should return 404 if object id is invalid', (done) => {
+        request(app)
+            .delete(`/todos/123123`)
+            .expect((res) => {
+                expect(404)
+            })
+            .end(done);
+    });
+});
