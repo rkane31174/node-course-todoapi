59cb9795099ed67a64277da9893913e559d168e9
Rkane <rkane@classlink.com>
2018-11-28 10:40:03 -0500

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -47,6 +47,16 @@ UserSchema.methods.generateAuthToken = function () {
         return token;
     });
 };
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+    return user.update({
+        $pull: {
+            tokens: {
+                token: token
+            }
+        }
+    });
+};
 
 UserSchema.statics.findByToken = function (token) {
     var User = this;
